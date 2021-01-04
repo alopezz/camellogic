b5b6ced7ce2fb8121b7ae2782a5d20b1ffb69f6a
Alex López <alex.lopez.zorzano@gmail.com>
2021-01-04 22:54:41 +0100

    Export formula in main module

--- src/camellogic.ml
+++ src/camellogic.ml
@@ -1,3 +1,13 @@
+type 'a formula = 'a Common.formula =
+  | And of 'a formula list
+  | Or of 'a formula list
+  | Not of 'a formula
+  | Implies of 'a formula * 'a formula
+  | Iff of 'a formula * 'a formula
+  | True
+  | False
+  | Atom of 'a
+
 let simplify = Common.simplify
 let nnf_of_formula = Common.nnf_of_formula
 let dnf_of_formula = Common.dnf_of_formula
@@ -12,8 +22,15 @@ let render = Propositional.render
 
 module IntTypes = struct
   type variable = Inttheory.variable
-  type term = Inttheory.term
-  type predicate = Inttheory.predicate
+  type term = Inttheory.term =
+    | Sum of term list
+    | Sub of term list
+    | Constant of int
+    | Mul of int * term
+    | Var of variable
+  type predicate = Inttheory.predicate =
+    | Equal of term * term
+    | GreaterThan of term * term
 end
 module IntTheoryLogic = Fol.IntTheoryLogic
 
